e978d1778dc44ba8322562e6ad7aaa0175144f82
jcomeme <hara@sunyou.co.jp>
2017-07-10 17:31:49 +0900

    now phase 4 / 10

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = this.transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 campos = target.transform.position + offset;
+		this.transform.position = Vector3.Lerp (this.transform.position, campos, smoothing * Time.deltaTime);
+	}
+}
